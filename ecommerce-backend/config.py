e5b3721c05048d9b2b5e734ad50c1b00f98c14dc
lobsterhandz <79618439+lobsterhandz@users.noreply.github.com>
2025-02-12 12:14:35 -0500

    feature fix v6.6

--- ecommerce-backend/config.py
+++ ecommerce-backend/config.py
@@ -63,7 +63,7 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.getenv("TEST_DATABASE_URL", "sqlite:///:memory:")
+    SQLALCHEMY_DATABASE_URI = os.getenv("sqlite:///:memory:")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     RATELIMIT_ENABLED = False # stop it from being enabled by default
     CACHE_TYPE = "NullCache"  # Disable caching during tests
